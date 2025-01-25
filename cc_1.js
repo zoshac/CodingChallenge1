cf05b47e3eb6b76d244bbe037d809938282c11c5
Zosha Chung <czosha@gmail.com>
2025-01-25 01:32:47 -0500

    Task 3- Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -8,7 +8,34 @@ console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
 // Employee Information 
 
-//Task 2 Primitive Data Types
+// Task 2 Primitive Data Types
 let productName = "mascara";
 const productPrice = 6.50;
 var isAvailable = true; 
+
+console.log(productName, typeof(productName));
+console.log(productPrice, typeof(productPrice));
+console.log(isAvailable, typeof(isAvailable));
+// Product Details 
+
+// Task 3 Number Data Type
+let accountBalance = 500.00;
+
+accountBalance += 50.00
+// Account Balance was increased by 50
+
+accountBalance -= 15.00
+// Account Balance was decreased by 15
+
+accountBalance *= 10;
+// Account Balance multiplied by 10
+
+accountBalance /= 2
+// Account Balance divided by 2
+
+console.log("Balance Increase:", accountBalance);
+console.log("Balance Decrease:", accountBalance);
+console.log("Balance Multiplied:", accountBalance);
+console.log("Balance Divided:", accountBalance);
+// Financial Transactions
+
