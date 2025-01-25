685b7e654c322b5e30ff3613dc2e9b6b3637527f
Zosha Chung <czosha@gmail.com>
2025-01-25 01:22:13 -0500

    Task 2- Product Info

--- cc_1.js
+++ cc_1.js
@@ -6,4 +6,9 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+// Employee Information 
 
+//Task 2 Primitive Data Types
+let productName = "mascara";
+const productPrice = 6.50;
+var isAvailable = true; 
