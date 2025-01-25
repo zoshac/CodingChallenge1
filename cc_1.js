9192efd9acc846c3dc16d3a15a53eeb6fdc591cf
Zosha Chung <czosha@gmail.com>
2025-01-25 01:19:35 -0500

    Task 1- Employee Info

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1 Variables and Data Types 
+let employeeName = "Maria Sanchez";
+const employeeID = 5072;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
